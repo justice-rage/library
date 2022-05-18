276a1474af9028b80612eeaab3a2a0dce199639e
justicepelteir <justice.douglas@hey.com>
2022-05-17 20:32:48 -0700

    create author.rb
    Co-authored-by: Deya Card <thedeyacard@gmail.com>
    Co-authored-by: Justice Douglas <justice.douglas@hey.com>

---
+++ lib/author.rb
@@ -0,0 +1,60 @@
+class Author
+  attr_reader :id
+  attr_accessor :name
+
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def self.all
+    returned_authors = DB.exec("SELECT * FROM authors;")
+    authors = []
+    returned_authors.each() do |author|
+      name = author.fetch("name")
+      id = author.fetch("id").to_i
+      authors.push(author.new({:name => name, :id => id}))
+    end
+    authors
+  end
+
+  def save
+    result = DB.exec("INSERT INTO authors (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def ==(author_to_compare)
+    self.name() == author_to_compare.name()
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM authors *;")
+  end
+
+  def self.find(id)
+    author = DB.exec("SELECT * FROM authors WHERE id = #{id};").first
+    if author
+      name = author.fetch("name")
+      id = author.fetch("id").to_i
+      author.new({:name => name, :id => id})
+    else
+      nil
+    end
+  end
+
+  def update(name)
+    @name = name
+    DB.exec("UPDATE authors SET name = '#{@name}' WHERE id = #{@id};")
+  end
+
+  def delete
+    DB.exec("DELETE FROM authors WHERE id = #{@id};")
+    DB.exec("DELETE FROM songs WHERE author_id = #{@id};")
+  end
+
+  def songs
+    Song.find_by_author(self.id)
+  end
+  
+end
